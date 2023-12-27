ef61aab7ff659ce3ff9ca88a3fdd002194b21f7a
Dhruv Mehta <64071840+dhruvmehtaaa@users.noreply.github.com>
2023-12-27 13:28:23 +0530

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,14 @@
 use webhook_flows::{create_endpoint, request_handler, send_response};
 use flowsnet_platform_sdk::logger;
 use std::collections::HashMap;
+use serde::{Deserialize, Serialize};
 use serde_json::Value;
+
+#[derive(Debug, Deserialize)]
+struct LoginRequest {
+    username: String,
+    password: String,
+}
 
 #[no_mangle]
 #[tokio::main(flavor = "current_thread")]
@@ -10,29 +17,43 @@ pub async fn on_deploy() {
 }
 
 #[request_handler]
-async fn handler(headers: Vec<(String, String)>, qry: HashMap<String, Value>, _body: Vec<u8>) {
+async fn handler(headers: Vec<(String, String)>, _qry: HashMap<String, Value>, body: Vec<u8>) {
     logger::init();
     log::info!("Headers -- {:?}", headers);
 
-    // Extract username and password from query parameters
-    let username = qry.get("username").and_then(|v| v.as_str()).unwrap_or("");
-    let password = qry.get("password").and_then(|v| v.as_str()).unwrap_or("");
+    // Deserialize JSON body into LoginRequest struct
+    let login_request: Result<LoginRequest, _> = serde_json::from_slice(&body);
+    
+    // Respond based on the deserialization result
+    match login_request {
+        Ok(request) => {
+            // Perform your login logic using request.username and request.password
+            let login_success = is_valid_login(&request.username, &request.password);
 
-    // Perform your login logic (replace this with your actual logic)
-    let login_success = is_valid_login(username, password);
+            // Respond based on the login result
+            let resp = if login_success {
+                "Login successful!\n"
+            } else {
+                "Login failed. Please check your username and password.\n"
+            };
 
-    // Respond based on the login result
-    let resp = if login_success {
-        "Login successful!\n"
-    } else {
-        "Login failed. Please check your username and password.\n"
-    };
+            send_response(
+                200,
+                vec![(String::from("content-type"), String::from("text/html"))],
+                resp.as_bytes().to_vec(),
+            );
+        }
+        Err(err) => {
+            log::error!("Failed to deserialize JSON body: {:?}", err);
 
-    send_response(
-        200,
-        vec![(String::from("content-type"), String::from("text/html"))],
-        resp.as_bytes().to_vec(),
-    );
+            // Respond with an error if JSON deserialization fails
+            send_response(
+                400,
+                vec![(String::from("content-type"), String::from("text/html"))],
+                b"Bad Request: Invalid JSON format.\n".to_vec(),
+            );
+        }
+    }
 }
 
 // Replace this with your actual login logic
