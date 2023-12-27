cbb7503279e7e5921d48778d1aeb576c0779297b
Dhruv Mehta <64071840+dhruvmehtaaa@users.noreply.github.com>
2023-12-27 13:36:14 +0530

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,63 +1,32 @@
-use webhook_flows::{create_endpoint, request_handler, send_response};
-use flowsnet_platform_sdk::logger;
-use std::collections::HashMap;
-use serde::{Deserialize, Serialize};
-use serde_json::Value;
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 
-#[derive(Debug, Deserialize)]
-struct LoginRequest {
+#[derive(serde::Deserialize)]
+struct LoginInfo {
     username: String,
     password: String,
 }
 
-#[no_mangle]
-#[tokio::main(flavor = "current_thread")]
-pub async fn on_deploy() {
-    create_endpoint().await;
+#[derive(serde::Serialize)]
+struct LoginResponse {
+    status: String,
 }
 
-#[request_handler]
-async fn handler(headers: Vec<(String, String)>, _qry: HashMap<String, Value>, body: Vec<u8>) {
-    logger::init();
-    log::info!("Headers -- {:?}", headers);
-
-    // Deserialize JSON body into LoginRequest struct
-    let login_request: Result<LoginRequest, _> = serde_json::from_slice(&body);
-    
-    // Respond based on the deserialization result
-    match login_request {
-        Ok(request) => {
-            // Perform your login logic using request.username and request.password
-            let login_success = is_valid_login(&request.username, &request.password);
-
-            // Respond based on the login result
-            let resp = if login_success {
-                "Login successful!\n"
-            } else {
-                "Login failed. Please check your username and password.\n"
-            };
-
-            send_response(
-                200,
-                vec![(String::from("content-type"), String::from("text/html"))],
-                resp.as_bytes().to_vec(),
-            );
-        }
-        Err(err) => {
-            log::error!("Failed to deserialize JSON body: {:?}", err);
-
-            // Respond with an error if JSON deserialization fails
-            send_response(
-                400,
-                vec![(String::from("content-type"), String::from("text/html"))],
-                b"Bad Request: Invalid JSON format.\n".to_vec(),
-            );
-        }
+fn login_handler(info: web::Json<LoginInfo>) -> impl Responder {
+    // Your login logic here
+    if info.username == "example_user" && info.password == "example_pass" {
+        HttpResponse::Ok().json(LoginResponse { status: "Login successful".to_string() })
+    } else {
+        HttpResponse::Unauthorized().json(LoginResponse { status: "Login failed".to_string() })
     }
 }
 
-// Replace this with your actual login logic
-fn is_valid_login(username: &str, password: &str) -> bool {
-    // Basic hardcoded validation (replace with real logic)
-    username == "admin" && password == "password"
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/login", web::post().to(login_handler))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
